86a860f0027b35325add7b709a8cea8d3e4daded
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-12-13 07:22:51 +0800

    new code

--- dropped_students.R
+++ dropped_students.R
@@ -1,5 +1,9 @@
-x <- c(1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000);
-y <- c(0.093, 0.080, 0.080, 0.083, 0.072, 0.090, 0.1211, 0.091, 0.080, 0.100, 0.010, 0.093, 0.075, 0.079, 0.098, 0.089, 0.091, 0.084, 0.087, 0.067);
+## Load the CSV file
+data = read.csv("/home/carlomigueldy/Desktop/FACT_TABLE.csv", header = TRUE)
+x <- c()
+y <- c()
+for(i in data[1]) { x <- i }
+for(i in data[3]) { y <- i }
 
 relation <- lm(y~x)
 
@@ -8,6 +12,9 @@ predictIt <- function(year) {
   result <-  predict(relation,a)
 }
 
-plot(x,y,col = "red" ,main = "Dropped Percentage of Students on Subject x For Year 1981 to 2000",
-     abline(lm(y~x)),cex = 1.3,pch = 16,xlab = "Year",ylab = "Percentage of Dropped Students")
+headerMessage <- "Dropped Students on Subject CS5 For Year 1981 to 2000"
+
+plot(x,y,col = "blue", main = headerMessage, 
+     abline(lm(y~x)), cex = 1.3,pch = 16
+     ,xlab = "Year", ylab = "Number of Dropped Students")
 
